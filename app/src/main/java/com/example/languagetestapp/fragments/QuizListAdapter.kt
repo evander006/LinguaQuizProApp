99caee1e6f7dae3d40bbdb2220e498c505ff6b2a
Anufrieva <anufrievaeva447@gmail.com>
2024-02-29 22:29:09 +0300

    Linqua Quiz Pro app

---
+++ app/src/main/java/com/example/languagetestapp/fragments/QuizListAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.languagetestapp.fragments
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.languagetestapp.R
+import com.example.languagetestapp.databinding.QuizItemRecViewBinding
+
+class QuizListAdapter(
+    private val quizModelList: List<QuizModel>
+) : RecyclerView.Adapter<QuizListAdapter.MyViewHolder>() {
+    private lateinit var navControl: NavController
+    class MyViewHolder(private val binding: QuizItemRecViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(model: QuizModel) {
+            binding.apply {
+                quizTitleText.text = model.title
+                quizSubtitleText.text = model.subtitle
+                quizTimeText.text = model.time + " min"
+                root.setOnClickListener {
+                    QuizFragment.questionModelList = model.questionList
+                    QuizFragment.timer=model.time
+                    it.findNavController().navigate(R.id.action_homeFragment_to_quizFragment)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = QuizItemRecViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return quizModelList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(quizModelList[position])
+
+    }
+}
+
